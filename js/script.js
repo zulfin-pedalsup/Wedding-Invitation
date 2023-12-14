0d8fe2f282f6a887cfdc5c9f3062f8cd838436a9
zulfin-pedalsup <zulfin.kureshi@pedalsup.com>
2023-12-14 22:20:46 +0530

    Zulfin invite

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ * @author Zulfin Kureshi <zmkureshi@gmail.com>
  */
 (function ($) {
     "use strict";
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2024 00:00:00").getTime();
+var countDownDate = new Date("Jan 21, 2024 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -85,9 +85,9 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 21th Jan, 2024!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Zulfin Kureshi', styles1, styles2);
 
 console.log(
     `%cShaadi me zaroor aana!\n\n`,
